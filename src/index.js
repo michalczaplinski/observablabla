df3d74390d36e658f743c84dfeeadc3531d0468f
Michal Czaplinski <mmczaplinski@gmail.com>
2019-01-15 15:15:17 -0500

    update examples

--- src/index.js
+++ src/index.js
@@ -5,55 +5,23 @@ import { observable, observe } from "./observablabla";
 import "./index.css";
 
 const store = observable({
+  seconds: 0,
+  updateSeconds: () => store.seconds++,
+
   number: 0,
   increment: () => store.number++,
   decrement: () => store.number--,
 
-  text: "hello",
-  updateText: text => (store.text = text),
-
-  list: [],
-  updateList: val => {
-    if (!val) return;
-    store.list = store.list.concat(val);
+  text: "",
+  updateText: text => {
+    store.text = text;
   }
 });
 
 const Number = observe(
   class Number extends Component {
     render() {
-      return <div> {store.number} </div>;
-    }
-  }
-);
-
-const List = observe(
-  class List extends Component {
-    state = { value: "" };
-
-    render() {
-      const { value } = this.state;
-      return (
-        <div>
-          <input
-            value={value}
-            onChange={e => this.setState({ value: e.target.value })}
-          />
-          <button
-            onClick={() => {
-              store.updateList(value);
-              this.setState({ value: "" });
-            }}
-          >
-            push
-          </button>
-          <ul>
-            {store.list.map((item, index) => (
-              <li key={index}> {item} </li>
-            ))}
-          </ul>
-        </div>
-      );
+      return <div> count : {store.number} </div>;
     }
   }
 );
@@ -62,11 +30,25 @@ const Text = observe(
   class Text extends Component {
     render() {
       return (
-        <input
-          value={store.text}
-          onChange={e => store.updateText(e.target.value)}
-        />
+        <div>
+          <input
+            value={store.text}
+            onChange={e => store.updateText(e.target.value)}
+          />
+          <div> {store.text} </div>
+        </div>
       );
+    }
+  }
+);
+
+const Seconds = observe(
+  class Seconds extends Component {
+    componentDidMount() {
+      setInterval(store.updateSeconds, 1000);
+    }
+    render() {
+      return <div> Seconds: {store.seconds} </div>;
     }
   }
 );
@@ -75,13 +57,15 @@ class App extends Component {
   render() {
     return (
       <div>
+        <Seconds />
+        <hr />
         <Text />
+        <hr />
         <div>
+          <Number />
           <button onClick={store.increment}> +1 </button>
           <button onClick={store.decrement}> -1 </button>
-          <Number />
         </div>
-        <List />
       </div>
     );
   }
--- src/observablabla.js
+++ src/observablabla.js
@@ -3,26 +3,30 @@ let currentlyRenderingComponent;
 
 export function observable(object) {
   const handler = {
-    get: function (target, key, receiver) {
+    get: function(target, key, receiver) {
       const componentMap = reactionsMap.get(target);
-      reactionsMap.set(target, { [key]: currentlyRenderingComponent, ...componentMap });
+      reactionsMap.set(target, {
+        [key]: currentlyRenderingComponent,
+        ...componentMap
+      });
       const result = Reflect.get(target, key, receiver);
       return result;
     },
 
-    set: function (target, key, value) {
+    set: function(target, key, value) {
       const componentMap = reactionsMap.get(target);
       if (!componentMap[key]) {
-        reactionsMap.set(target, { [key]: currentlyRenderingComponent, ...componentMap });
+        reactionsMap.set(target, {
+          [key]: currentlyRenderingComponent,
+          ...componentMap
+        });
         currentlyRenderingComponent.forceUpdate();
         return Reflect.set(target, key, value);
       }
-      console.log(reactionsMap);
       componentMap[key].forceUpdate();
       return Reflect.set(target, key, value);
     }
   };
-
   return new Proxy(object, handler);
 }
 
