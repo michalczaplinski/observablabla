ee615f11c22accfcfd7a1a617272a6ef87924922
Michal Czaplinski <mmczaplinski@gmail.com>
2019-01-16 18:41:07 -0500

    add commnents

--- src/observablabla.js
+++ src/observablabla.js
@@ -3,10 +3,26 @@
 
 const handler = {
   get: (target, key) => {
+    // if (typeof currentlyRenderingComponent === "undefined") {
+    //   return Reflect.get(target, key);
+    // }
+    // if (!reactionsMap[key]) {
+    //   reactionsMap[key] = [currentlyRenderingComponent];
+    // }
+    // const hasComponent = reactionsMap[key].find(
+    //   comp => comp.ID === currentlyRenderingComponent.ID
+    // );
+    // if (!hasComponent) {
+    //   reactionsMap[key].push(currentlyRenderingComponent);
+    // }
     console.log(`accessing key: ${key}`);
     return Reflect.get(target, key);
   },
   set: (target, key, value) => {
+    // if (!reactionsMap[key]) {
+    //   reactionsMap[key] = [currentlyRenderingComponent];
+    // }
+    // reactionsMap[key].forEach(component => component.forceUpdate())
     console.log(`setting key ${key} to value ${value}`);
     return Reflect.set(target, key, value);
   }
