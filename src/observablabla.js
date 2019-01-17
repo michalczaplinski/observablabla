880cf136ad1a35b3c92e6dab347214c4045ff4f0
Michal Czaplinski <mmczaplinski@gmail.com>
2019-01-17 16:05:26 -0500

    final version

--- src/observablabla.js
+++ src/observablabla.js
@@ -1,19 +1,32 @@
-let currentlyRenderingComponent;
 const reactionsMap = {};
+let currentlyRenderingComponent;
 
 const handler = {
-  get: (target, key) => {
-    if (typeof target[key] === "function") {
+  get: function(target, key) {
+    if (
+      typeof currentlyRenderingComponent === "undefined" ||
+      typeof target[key] === "function"
+    ) {
       return target[key];
     }
     if (!reactionsMap[key]) {
-      reactionsMap[key] = currentlyRenderingComponent;
+      reactionsMap[key] = [currentlyRenderingComponent];
+    }
+    const hasComponent = reactionsMap[key].find(
+      comp => comp.ID === currentlyRenderingComponent.ID
+    );
+    if (!hasComponent) {
+      reactionsMap[key].push(currentlyRenderingComponent);
     }
     return target[key];
   },
-  set: (target, key, value) => {
+
+  set: function(target, key, value) {
+    if (!reactionsMap[key]) {
+      reactionsMap[key] = [currentlyRenderingComponent];
+    }
+    reactionsMap[key].forEach(component => component.forceUpdate());
     target[key] = value;
-    reactionsMap[key].forceUpdate();
     return true;
   }
 };
@@ -24,63 +37,16 @@ export function store(object) {
 
 export function view(MyComponent) {
   return class Observer extends MyComponent {
+    ID = `${Math.floor(Math.random() * 10e9)}`;
     static displayName = `${MyComponent.name}__Observer`;
-    name = `${MyComponent.name}__Observer`;
+
     render() {
       currentlyRenderingComponent = this;
-      return super.render();
+      const renderValue = super.render();
+      currentlyRenderingComponent = undefined;
+      return renderValue;
     }
   };
 }
 
 window.reactionsMap = reactionsMap;
-
-// const reactionsMap = {};
-// let currentlyRenderingComponent;
-
-// const handler = {
-//   get: function(target, key) {
-//     if (typeof currentlyRenderingComponent === "undefined") {
-//       return target[key];
-//     }
-//     if (!reactionsMap[key]) {
-//       reactionsMap[key] = [currentlyRenderingComponent];
-//     }
-//     const hasComponent = reactionsMap[key].find(
-//       comp => comp.ID === currentlyRenderingComponent.ID
-//     );
-//     if (!hasComponent) {
-//       reactionsMap[key].push(currentlyRenderingComponent);
-//     }
-//     return target[key];
-//   },
-
-//   set: function(target, key, value) {
-//     if (!reactionsMap[key]) {
-//       reactionsMap[key] = [currentlyRenderingComponent];
-//     }
-//     reactionsMap[key].forEach(component => component.forceUpdate());
-//     target[key] = value;
-//     return true;
-//   }
-// };
-
-// export function store(object) {
-//   return new Proxy(object, handler);
-// }
-
-// export function view(MyComponent) {
-//   return class Observer extends MyComponent {
-//     ID = `${Math.floor(Math.random() * 10e9)}`;
-//     static displayName = `${MyComponent.name}__Observer`;
-
-//     render() {
-//       currentlyRenderingComponent = this;
-//       const renderValue = super.render();
-//       currentlyRenderingComponent = undefined;
-//       return renderValue;
-//     }
-//   };
-// }
-
-// window.reactionsMap = reactionsMap;
