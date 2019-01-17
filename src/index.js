da0e9cd33698b836b87e21bfd5e6a89d7859f8cb
Michal Czaplinski <mmczaplinski@gmail.com>
2019-01-16 22:40:21 -0500

    update the branch to inial state

--- src/index.js
+++ src/index.js
@@ -5,17 +5,17 @@ import { store, view } from "./observablabla";
 import "./index.css";
 
 const state = store({
-  seconds: 0,
-  updateSeconds: () => state.seconds++,
+  // seconds: 0,
+  // updateSeconds: () => state.seconds++,
 
   number: 0,
   increment: () => state.number++,
-  decrement: () => state.number--,
+  decrement: () => state.number--
 
-  text: "",
-  updateText: text => {
-    state.text = text;
-  }
+  // text: "",
+  // updateText: text => {
+  //   state.text = text;
+  // }
 });
 
 const Number = view(
@@ -26,49 +26,49 @@ const Number = view(
   }
 );
 
-const Text = view(
-  class Text extends Component {
-    render() {
-      return (
-        <div>
-          <input
-            value={state.text}
-            onChange={e => state.updateText(e.target.value)}
-          />
-          <div> {state.text} </div>
-        </div>
-      );
-    }
-  }
-);
+// const Text = view(
+//   class Text extends Component {
+//     render() {
+//       return (
+//         <div>
+//           <input
+//             value={state.text}
+//             onChange={e => state.updateText(e.target.value)}
+//           />
+//           <div> {state.text} </div>
+//         </div>
+//       );
+//     }
+//   }
+// );
 
-const Seconds = view(
-  class Seconds extends Component {
-    componentDidMount() {
-      setInterval(() => state.updateSeconds(), 1000);
-    }
-    render() {
-      return <div> Seconds: {state.seconds} </div>;
-    }
-  }
-);
+// const Seconds = view(
+//   class Seconds extends Component {
+//     componentDidMount() {
+//       setInterval(() => state.updateSeconds(), 1000);
+//     }
+//     render() {
+//       return <div> Seconds: {state.seconds} </div>;
+//     }
+//   }
+// );
 
 const App = view(
   class App extends Component {
     render() {
       return (
         <div>
-          <Seconds />
+          {/* <Seconds />
           <hr />
           <Text />
           <hr />
           <Number />
           <hr />
-          <div>
-            <Number />
-            <button onClick={() => state.increment()}> +1 </button>
-            <button onClick={() => state.decrement()}> -1 </button>
-          </div>
+          <div> */}
+          <Number />
+          <button onClick={() => state.increment()}> +1 </button>
+          <button onClick={() => state.decrement()}> -1 </button>
+          {/* </div> */}
         </div>
       );
     }
