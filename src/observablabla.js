4524857b379f358f866c53323629d9771752633c
Michal Czaplinski <mmczaplinski@gmail.com>
2019-01-17 16:35:58 -0500

    final final final

--- src/observablabla.js
+++ src/observablabla.js
@@ -1,5 +1,15 @@
+const handler = {
+  get: (target, key) => {
+    return target[key];
+  },
+  set: (target, key, value) => {
+    target[key] = value;
+    return true;
+  }
+};
+
 export function store(object) {
-  return object;
+  return new Proxy(object, handler);
 }
 
 export function view(MyComponent) {
