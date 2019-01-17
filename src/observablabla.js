125169e7580061060edbede2f6afe463fd3c6fb4
Michal Czaplinski <mmczaplinski@gmail.com>
2019-01-17 15:14:43 -0500

    init

--- src/observablabla.js
+++ src/observablabla.js
@@ -1,27 +1,27 @@
-// let currentlyRenderingComponent;
+let currentlyRenderingComponent;
 
-// const handler = {
-//   get: (target, key) => {
-//     console.log(`${key}  ->  ${currentlyRenderingComponent.name}`);
-//     return target[key];
-//   },
-//   set: (target, key, value) => {
-//     target[key] = value;
-//     return true;
-//   }
-// };
+const handler = {
+  get: (target, key) => {
+    console.log(`${key}  ->  ${currentlyRenderingComponent.name}`);
+    return target[key];
+  },
+  set: (target, key, value) => {
+    target[key] = value;
+    return true;
+  }
+};
 
-// export function store(object) {
-//   return new Proxy(object, handler);
-// }
+export function store(object) {
+  return new Proxy(object, handler);
+}
 
-// export function view(MyComponent) {
-//   return class Observer extends MyComponent {
-//     static displayName = `${MyComponent.name}__Observer`;
-//     name = `${MyComponent.name}__Observer`;
-//     render() {
-//       currentlyRenderingComponent = this;
-//       return super.render();
-//     }
-//   };
-// }
+export function view(MyComponent) {
+  return class Observer extends MyComponent {
+    static displayName = `${MyComponent.name}__Observer`;
+    name = `${MyComponent.name}__Observer`;
+    render() {
+      currentlyRenderingComponent = this;
+      return super.render();
+    }
+  };
+}
