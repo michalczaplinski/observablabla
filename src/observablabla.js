bc423c6877476faec4249e0b0fac11136fac00b2
Michal Czaplinski <mmczaplinski@gmail.com>
2019-01-17 16:55:58 -0500

    final

--- src/observablabla.js
+++ src/observablabla.js
@@ -26,22 +26,34 @@ export function view(MyComponent) {
   };
 }
 
-// let currentlyRenderingComponent;
 // const reactionsMap = {};
+// let currentlyRenderingComponent;
 
 // const handler = {
-//   get: (target, key) => {
-//     if (typeof target[key] === "function") {
+//   get: function (target, key) {
+//     if (
+//       typeof currentlyRenderingComponent === "undefined"
+//     ) {
 //       return target[key];
 //     }
 //     if (!reactionsMap[key]) {
-//       reactionsMap[key] = currentlyRenderingComponent;
+//       reactionsMap[key] = [currentlyRenderingComponent];
+//     }
+//     const hasComponent = reactionsMap[key].find(
+//       comp => comp.ID === currentlyRenderingComponent.ID
+//     );
+//     if (!hasComponent) {
+//       reactionsMap[key].push(currentlyRenderingComponent);
 //     }
 //     return target[key];
 //   },
-//   set: (target, key, value) => {
+
+//   set: function (target, key, value) {
+//     if (!reactionsMap[key]) {
+//       reactionsMap[key] = [currentlyRenderingComponent];
+//     }
+//     reactionsMap[key].forEach(component => component.forceUpdate());
 //     target[key] = value;
-//     reactionsMap[key].forceUpdate();
 //     return true;
 //   }
 // };
@@ -52,11 +64,14 @@ export function view(MyComponent) {
 
 // export function view(MyComponent) {
 //   return class Observer extends MyComponent {
+//     ID = `${Math.floor(Math.random() * 10e9)}`;
 //     static displayName = `${MyComponent.name}__Observer`;
-//     name = `${MyComponent.name}__Observer`;
+
 //     render() {
 //       currentlyRenderingComponent = this;
-//       return super.render();
+//       const renderValue = super.render();
+//       currentlyRenderingComponent = undefined;
+//       return renderValue;
 //     }
 //   };
 // }
